233ac9551c79df5864b655d293b03118e9827a13
Kaito Maeshiro <e185722@ie.u-ryukyu.ac.jp>
2018-11-26 09:35:15 +0900

    Step2

--- src/Report4.java
+++ src/Report4.java
@@ -1,7 +1,14 @@
 public class Report4 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+
+        try{
+            String str = null;
+            System.out.println(str.length());
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw new NullPointerException("0で割ったときの例外を発生させる");
+        }
     }
 }
 
