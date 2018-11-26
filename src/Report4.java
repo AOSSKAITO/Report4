462744dd7d0ee5ad81369a02fca6dae402414ca9
Kaito Maeshiro <e185722@ie.u-ryukyu.ac.jp>
2018-11-26 14:54:30 +0900

    Step3

--- src/Report4.java
+++ src/Report4.java
@@ -1,4 +1,4 @@
-public class Report4 {
+/*public class Report4 {
     public static void main(String[] args) {
 
         try{
@@ -9,6 +9,13 @@ public class Report4 {
             System.out.println(e.getMessage());
             throw new NullPointerException("NullPointerExceptionが発生しました");
         }
+    }
+}*/
+
+public class Report4 {
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
 
